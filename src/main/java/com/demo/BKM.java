0d4d5e1384876b881506b1d31379c250f4e5e53b
Donato Marrazzo <donato.marrazzo@gmail.com>
2019-11-04 16:36:58 +0100

    drl invocation

--- src/main/java/com/demo/BKM.java
+++ src/main/java/com/demo/BKM.java
@@ -1,11 +1,33 @@
 package com.demo;
 
+import java.util.Collection;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
 /**
  * BKM
  */
 public class BKM {
+    private static KieServices ks = KieServices.Factory.get();
+    private static KieContainer kieContainer = ks.getKieClasspathContainer();
 
     public static double xlsKnowledge(String season) {
-        return 0.9;
+        double result = 0.9;
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.insert(season);
+
+        kieSession.fireAllRules();
+
+        Collection<? extends Object> objects = kieSession.getObjects();
+
+        for (Object object : objects) {
+            if (object instanceof Double)
+                result = (Double) object;
+        }
+        kieSession.dispose();
+
+        return result;
     }
 }
