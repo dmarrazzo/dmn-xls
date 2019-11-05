c0b60eea35d167328e2aedddd3fd142d4197dde8
Donato Marrazzo <donato.marrazzo@gmail.com>
2019-11-05 12:45:06 +0100

    debugging

--- src/main/java/com/demo/BKM.java
+++ src/main/java/com/demo/BKM.java
@@ -1,10 +1,16 @@
 package com.demo;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+import org.drools.core.ClassObjectFilter;
 import org.kie.api.KieServices;
+import org.kie.api.command.Command;
+import org.kie.api.runtime.ExecutionResults;
 import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
+import org.kie.internal.command.CommandFactory;
 
 /**
  * BKM
@@ -15,20 +21,16 @@ public class BKM {
 
     public static double xlsKnowledge(String season) {
         double result = 0.9;
-        KieSession kieSession = kieContainer.newKieSession();
-        kieSession.insert(season);
-
-        int fireAllRules = kieSession.fireAllRules();
-        System.out.println("fireAllRules: "+fireAllRules);
-
-        Collection<? extends Object> objects = kieSession.getObjects();
-
-        for (Object object : objects) {
-            if (object instanceof Double)
-                result = (Double) object;
+        StatelessKieSession kieSession = kieContainer.newStatelessKieSession();
+        List<Command> cmds = new ArrayList();
+        cmds.add(CommandFactory.newInsert(season, "season"));
+        cmds.add(CommandFactory.newFireAllRules());
+        cmds.add(CommandFactory.newGetObjects(new ClassObjectFilter(Double.class), "doubles"));
+        ExecutionResults results = kieSession.execute(CommandFactory.newBatchExecution(cmds));
+        Collection<?> doubleCollection = (Collection<?>) results.getValue("doubles");
+        for (Object object : doubleCollection) {
+            result = (Double) object;
         }
-        kieSession.dispose();
-
         return result;
     }
 }
